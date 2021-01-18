59b8493c95f25e0cb0f959c242169c99e20d8348
rozetta1125 <rozetta1125@gmail.com>
2021-01-18 23:33:05 +0700

    day 3 progress v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import React from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import PageNotFound from "./components/PageNotFound";
 import DataPage from "./components/DataPage";
@@ -9,14 +9,14 @@ import ManageData from './components/ManageData';
 function App() {
   return (
     <div id="app" className="container-fluid">
-      <BrowserRouter>
+      <HashRouter>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path="/data" component={DataPage} />
         <Route path="/detail/:id" component={ManageData} />
         <Route component={PageNotFound} />
       </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
